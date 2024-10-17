308737226c855fc8477b5be41757f92810a31595
Andreas Rayias <wowcaeu.andreas@gmail.com>
2024-10-18 00:39:17 +0100

    seperate front end and backend

---
+++ server/src/game/game.ts
@@ -0,0 +1,7 @@
+// class Game {
+//   players: Player[];
+//   constructor() {
+//     this.players = [];
+//     this.board = new Board();
+//   }
+// }
--- src/game/game.ts
+++
@@ -1,7 +0,0 @@
-class Game {
-  players: Player[];
-  constructor() {
-    this.players = [];
-    this.board = new Board();
-  }
-}
