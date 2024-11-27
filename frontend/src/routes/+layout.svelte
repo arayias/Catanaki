8690497e9e2bfdc5da9ffbbe2acfad61ce961692
Andreas Rayias <wowcaeu.andreas@gmail.com>
2024-11-27 03:49:59 +0000

    add toast pop ups

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,7 +1,9 @@
 <script>
 	import '../app.css';
+	import { SvelteToast } from '@zerodevx/svelte-toast';
 </script>
 
 <div class="mx-auto my-auto flex h-[90vh] w-[90%] flex-col items-center justify-center">
 	<slot></slot>
+	<SvelteToast></SvelteToast>
 </div>
